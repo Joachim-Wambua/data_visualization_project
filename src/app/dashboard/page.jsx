68c639d013a8686556db93b9b10632fe8e75ddf7
Joachim-Wambua <kimwambua96@gmail.com>
2025-04-08 16:27:31 +0300

    initial commit

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,16 @@
-import ChartCard from '@/components/SteelProductionChart';
-import { getEmissionsData } from '@/lib/getAnnualSteelProdData';
+import ChartCard from "@/components/SteelProductionChart";
+import { getEmissionsData } from "@/lib/getAnnualSteelProdData";
 
 export default function DashboardPage() {
-  const data = getEmissionsData();
+  //   const data = getEmissionsData();
 
   return (
     <main className="min-h-screen bg-gray-50 p-8">
-      <h1 className="text-2xl font-bold mb-6 text-gray-900">Steel Emissions Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-6 text-gray-900">
+        Steel Emissions Dashboard
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <ChartCard title="Global Steel Sector CO₂ Emissions (2016–2022)" data={data} />
+        {/* <ChartCard title="Global Steel Sector CO₂ Emissions (2016–2022)" data={data} /> */}
       </div>
     </main>
   );
